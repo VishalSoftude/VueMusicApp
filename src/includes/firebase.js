5ac6d840dc4a16aaa7868106c42965a35c320fa3
puneetshinde.systematix@hotmail.com <puneetshinde.systematix@hotmail.com>
2023-07-12 12:13:16 +0530

    media-upload

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,26 +1,22 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-  const firebaseConfig = {
+import 'firebase/storage'
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpSj5wtlMYqjxIxv2VJFrs38DKteEy9YA',
+  authDomain: 'musicapp-3d9c5.firebaseapp.com',
+  projectId: 'musicapp-3d9c5',
+  storageBucket: 'musicapp-3d9c5.appspot.com',
+  messagingSenderId: '92101047963',
+  appId: '1:92101047963:web:399028e1a5324104c4f83e'
+}
 
-    apiKey: "AIzaSyDpSj5wtlMYqjxIxv2VJFrs38DKteEy9YA",
-    authDomain: "musicapp-3d9c5.firebaseapp.com",
-    projectId: "musicapp-3d9c5",
-    storageBucket: "musicapp-3d9c5.appspot.com",
-    messagingSenderId: "92101047963",
-    appId: "1:92101047963:web:399028e1a5324104c4f83e"
-  
-  };
-    
-   firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-   const auth = firebase.auth();
-   const db = firebase.firestore();
+const auth = firebase.auth()
+const db = firebase.firestore()
+const storage = firebase.storage()
 
-   const usersCollection = db.collection('users')
+const usersCollection = db.collection('users')
 
-   export {
-    auth,
-    db,
-    usersCollection
-   }
+export { auth, db, usersCollection, storage }
